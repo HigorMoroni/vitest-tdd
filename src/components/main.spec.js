17aac2c69dcab740737985cfcabf68d69b99ddca
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-13 13:36:58 -0300

    feat: use chai/vitest methos

--- src/components/main.spec.js
+++ src/components/main.spec.js
@@ -1,4 +1,12 @@
-import { afterAll, afterEach, beforeAll, beforeEach, describe, it } from 'vitest';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it
+} from 'vitest';
 
 // references
 //.skip - ignorar um ou um conjunto de testes
@@ -9,31 +17,47 @@ import { afterAll, afterEach, beforeAll, beforeEach, describe, it } from 'vitest
 
 describe('Main', function() {
   describe('Method A', function() {
-    it.skip('should happen teestinnnn', function() {
+    it.skip('should this when there', function() {
       // espera que aconteça
       // Entrada de dados / método
       // Espera retornar
     });
   });
   describe.only('Method A', function() {
+    let arr;
+
     beforeAll(function() {
       console.log('beforeAll');
     });
     beforeEach(function() {
-      console.log('beforeEach');
+      arr = [1, 2, 3];
     });
-    it('should happen 1', function() {
-      console.log('teste1');
+
+    // Smoke Test (testa se a função existe, tipo de dados, etc)
+    it('should be an array', function() {
+      expect(arr).to.be.an('array');
     });
-    it('should happen 2', function() {
-      console.log('teste2');
+    it('should have a size of 4 when push another value to the array', function() {
+      arr.push(4);
+      //expect(arr).to.have.lengthOf(4);
+      //expect(arr).have.lengthOf(4);
+      expect(arr).toHaveLength(4);
     });
-    it('should happen 3', function() {
-      console.log('teste3');
+    it('should remove the value 3 when use pop in the array', function() {
+      arr.pop();
+      //expect(arr).to.not.include(3);
+      //expect(arr).not.include(3);
+      expect(arr).not.toContain(3);
     });
-    it('should happen 4', function() {
-      console.log('teste4');
+    it('should have a size of 2 when pop a value from the array', function() {
+      arr.pop();
+      expect(arr).toHaveLength(2);
     });
+    it('should return true when pop 3 from the array', function() {
+      //expect(arr.pop() === 3).to.be.true;
+      expect(arr.pop() === 3).toBe(true);
+    });
+
     afterEach(function() {
       console.log('afterEach');
     });
