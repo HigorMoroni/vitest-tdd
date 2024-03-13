f40a2c63542a84ed9e59ddfc9e67aedacb7739f8
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-13 16:27:32 -0300

    feat: finish calculadora lib tests

--- src/components/calculadora/calculadora.js
+++ src/components/calculadora/calculadora.js
@@ -1,6 +1,14 @@
 export default {
-  sum() {},
-  sub() {},
-  mult() {},
-  div() {}
+  sum(num1, num2) {
+    return num1 + num2;
+  },
+  sub(num1, num2) {
+    return num1 - num2;
+  },
+  mult(num1, num2) {
+    return num1 * num2;
+  },
+  div(num1, num2) {
+    return num2 === 0 ? 'Não é possível dividir por zero!': num1 / num2;
+  }
 };
--- src/components/calculadora/calculadora.spec.js
+++ src/components/calculadora/calculadora.spec.js
@@ -12,6 +12,7 @@ describe('Calculadora', () => {
       //assert.exists(calc);
       expect(calc).to.exist;
     });
+
     it('should exist the method "sum"', () => {
       expect(calc.sum).to.exist;
 
@@ -29,6 +30,38 @@ describe('Calculadora', () => {
     it('should exist the method "div"', () => {
       expect(calc.div).to.exist;
       expect(calc.div).to.be.a('function');
+    });
+  });
+
+  describe('Sum', () => {
+    it('should return 4 when "sum(2,2)"', () => {
+      //expect(calc.sum(2, 2)).to.be.equal(4);
+      //expect(calc.sum(2, 2)).toEqual(4);
+      expect(calc.sum(2, 2)).toBe(4);
+    });
+  });
+
+  describe('Sub', () => {
+    it('should return 4 when "sub(6,2)"', () => {
+      expect(calc.sub(6, 2)).toBe(4);
+    });
+    it('should return -4 when "sub(6,10)"', () => {
+      expect(calc.sub(6, 10)).toBe(-4);
+    });
+  });
+
+  describe('Mult', () => {
+    it('should return 4 when "mult(2,2)"', () => {
+      expect(calc.mult(2, 2)).toBe(4);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 2 when "div(4,2)"', () => {
+      expect(calc.div(4, 2)).toBe(2);
+    });
+    it('should return a error message when is a division by zero', () => {
+      expect(calc.div(4, 0)).toBe('Não é possível dividir por zero!');
     });
   });
 });
