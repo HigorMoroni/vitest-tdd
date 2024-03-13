e099b995174529ca6595a81f451aeee79f1e6f93
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-13 17:07:47 -0300

    fix: refector safety the FizzBuzz function

--- src/components/FizzBuzz/fizzBuzz.js
+++ src/components/FizzBuzz/fizzBuzz.js
@@ -1,9 +1,11 @@
 const FizzBuzz = num => {
-  if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-  if(num % 3 === 0) return 'Fizz';
-  if(num % 5 === 0) return 'Buzz';
+  // if(num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  // if(num % 3 === 0) return 'Fizz';
+  // if(num % 5 === 0) return 'Buzz';
 
-  return num;
+  // return num;
+
+  return ((num % 3 === 0 ? 'Fizz': '') + (num % 5 === 0 ? 'Buzz': '')) || num;
 };
 
 export default FizzBuzz;
