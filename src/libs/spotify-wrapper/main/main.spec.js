6cd72721ac611e200e55de4a6d687c5ec24fd951
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-19 18:05:05 -0300

    feat: using mock restore and call types

--- src/libs/spotify-wrapper/main/main.spec.js
+++ src/libs/spotify-wrapper/main/main.spec.js
@@ -1,4 +1,10 @@
-import { describe, expect, it, vi } from 'vitest';
+import {
+  describe,
+  expect,
+  it,
+  vi,
+  beforeEach
+} from 'vitest';
 import {
   search,
   searchAlbums,
@@ -36,23 +42,31 @@ describe('Spotify Wrapper', () => {
   });
 
   describe('Generic Search', () => {
-    it('should call fetch function', () => {
-      global.fetch = vi.fn();
-      search();
+    global.fetch = vi.fn();
 
+    beforeEach(function() {
+      fetch.mockRestore();
+    });
+
+    it('should call fetch function', () => {
+      search();
       expect(fetch).toHaveBeenCalledTimes(1);
     });
 
-    it('should receive the correct url to fetch', () => {
-      search('Alok', 'artist');
+    describe('should receive the correct URL to fetch', () => {
+      it('passing one type', () => {
+        search('Alok', 'artist');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist');
+  
+        search('Elepunk', 'album');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist'); // Testando que o toBeCalledWith não se refere a ultima chamada!
+        expect(fetch).toHaveBeenLastCalledWith('https://api.spotify.com/v1/search?q=Elepunk&type=album');
+      });
 
-      expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist');
-
-
-      search('Elepunk', 'album');
-
-      expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Elepunk&type=album');
-
+      it('passing two or more types', () => {
+        search('Metallica', ['artist', 'album', 'playlist']);
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Metallica&type=artist,album,playlist');
+      });
     });
   });
 });
