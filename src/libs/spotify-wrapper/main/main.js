00ba5806208744c6e9aac58c1805c31c7fab25db
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-20 12:55:10 -0300

    feat: finish search methods

--- src/libs/spotify-wrapper/main/main.js
+++ src/libs/spotify-wrapper/main/main.js
@@ -1,9 +1,11 @@
-export const search = (query, type) => {
-  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then(data => data.json());
-};
-export const searchAlbums = () => {};
-export const searchArtists = () => {};
-export const searchTracks = () => {};
-export const searchPlaylists = () => {};import spotifyAccessAuth from '@/auth/spotify-access-auth';
 import spotifyAccessAuth from '@/auth/spotify-access-auth';
+
+export const search = (query, type) => 
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, {
+    headers: spotifyAccessAuth
+  }).then(data => data.json());
+    
+export const searchAlbums = (query) => search(query, 'album');
+export const searchArtists = (query) => search(query, 'artist');
+export const searchTracks = (query) => search(query, 'track');
+export const searchPlaylists = (query) => search(query, 'playlist');
--- src/libs/spotify-wrapper/main/main.spec.js
+++ src/libs/spotify-wrapper/main/main.spec.js
@@ -44,32 +44,106 @@ describe('Spotify Wrapper', () => {
     });
   });
 
-  describe('Generic Search', () => {
+  describe('Search Methods', () => {
     beforeEach(() => {
       global.fetch = vi.fn().mockImplementation(returnsPromise);
     });
-
+  
     afterEach(() => fetch.mockRestore());
-
-    it('should call fetch function', () => {
-      search();
-      expect(fetch).toHaveBeenCalledTimes(1);
+  
+    describe('Generic Search', () => {
+      it('should call fetch function', () => {
+        search();
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+  
+      describe('should receive the correct URL to fetch', () => {
+        it('passing one type', () => {
+          search('Alok', 'artist');
+          expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist');
+    
+          search('Elepunk', 'album');
+          expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist'); // Testando que o toBeCalledWith não se refere a ultima chamada!
+          expect(fetch).toHaveBeenLastCalledWith('https://api.spotify.com/v1/search?q=Elepunk&type=album');
+        });
+  
+        it('passing two or more types', () => {
+          search('Metallica', ['artist', 'album', 'playlist']);
+          expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Metallica&type=artist,album,playlist');
+        });
+      });
+  
+      it('should return the JSON Fata from the', async () => {
+        // TODO - class  95
+  
+        const artist = await search('Alok', 'artist');
+        expect(artist).toStrictEqual({ data: 'data' });
+      });
     });
 
-    describe('should receive the correct URL to fetch', () => {
-      it('passing one type', () => {
-        search('Alok', 'artist');
-        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist');
-  
-        search('Elepunk', 'album');
-        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Alok&type=artist'); // Testando que o toBeCalledWith não se refere a ultima chamada!
-        expect(fetch).toHaveBeenLastCalledWith('https://api.spotify.com/v1/search?q=Elepunk&type=album');
+    describe('Search Artists', () => {
+      it('should call fetch function', () => {
+        searchArtists('Anitta');
+        
+        expect(fetch).toHaveBeenCalledTimes(1);
       });
 
-      it('passing two or more types', () => {
-        search('Metallica', ['artist', 'album', 'playlist']);
-        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Metallica&type=artist,album,playlist');
+      it('should call fetch with the correct URL', () => {
+        searchArtists('Projota');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Projota&type=artist');
+
+        searchArtists('Muse');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Muse&type=artist');
+      });
+    });
+
+    describe('Search Albums', () => {
+      it('should call fetch function', () => {
+        searchAlbums('Encontros');
+        
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call fetch with the correct URL', () => {
+        searchAlbums('SOUR');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=SOUR&type=album');
+
+        searchAlbums('PIRATA');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=PIRATA&type=album');
+      });
+    });
+
+    describe('Search Tracks', () => {
+      it('should call fetch function', () => {
+        searchTracks('traitor');
+        
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call fetch with the correct URL', () => {
+        searchTracks('Leão');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Leão&type=track');
+
+        searchTracks('Medicina');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Medicina&type=track');
+      });
+    });
+
+    describe('Search Playlists', () => {
+      it('should call fetch function', () => {
+        searchPlaylists('2024');
+        
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call fetch with the correct URL', () => {
+        searchPlaylists('Melhores');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Melhores&type=playlist');
+
+        searchPlaylists('Pagodão');
+        expect(fetch).toBeCalledWith('https://api.spotify.com/v1/search?q=Pagodão&type=playlist');
       });
     });
   });
+
 });
