0fcd58c34d598ca007893c359c233f500dc2903a
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-20 15:40:14 -0300

    feat: add ion icons

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => ['ion-icon'].includes(tag),
+      }
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
