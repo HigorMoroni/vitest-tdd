4c8ed8756c19b46e94e843837391eed5b32b25f7
Higor Moroni <higor.ferreira@melhorenvio.com>
2024-03-20 17:48:21 -0300

    feat: create all album functions

--- src/libs/spotify-wrapper/album/album.spec.js
+++ src/libs/spotify-wrapper/album/album.spec.js
@@ -2,7 +2,7 @@
 // getAlbumTracks
 
 import { describe, expect, it, beforeEach, afterEach, vi } from 'vitest';
-import { getAlbum, getAlbumTracks } from './index';
+import { getAlbum, getAlbums, getAlbumTracks } from './index';
 
 import { returnsPromise } from '../../../utils/mockImplementations';
 import spotifyAccessAuth from '../../../auth/spotify-access-auth';
@@ -43,6 +43,48 @@ describe('Album', () => {
 
       it('should return the correct data from Promise', async () => {
         const album = await getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+        expect(album).toStrictEqual({ data: 'data' });
+      });
+    });
+
+    describe('Get Albums Method', () => {
+      const albunsIdArray = ['382ObEPsp2rxGrnsizN5TX', '1A2GTWGtFfWp7KSQTwWOyo', '2noRn2Aes5aoNVsU6iWThc'];
+
+      it('should call fetch method', () => {
+        getAlbums();
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call fetch with the correct URL', () => {
+        getAlbums(albunsIdArray);
+        expect(fetch).toHaveBeenCalledWith(
+          'https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc',
+          fetchOptions
+        );
+      });
+
+      it('should return the correct data from Promise', async () => {
+        const album = await getAlbums(['382ObEPsp2rxGrnsizN5TX', '1A2GTWGtFfWp7KSQTwWOyo', '2noRn2Aes5aoNVsU6iWThc']);
+        expect(album).toStrictEqual({ data: 'data' });
+      });
+    });
+
+    describe('Get Album Track Method', () => {
+      it('should call fetch method', () => {
+        getAlbumTracks();
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call fetch with the correct URL', () => {
+        getAlbumTracks('4aawyAB9vmqN3uQ7FjRGTy');
+        expect(fetch).toHaveBeenCalledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks', fetchOptions);
+
+        getAlbumTracks('4aawyAB9vmqN3uQ7FjRGTk');
+        expect(fetch).toHaveBeenCalledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTk/tracks', fetchOptions);
+      });
+
+      it('should return the correct data from Promise', async () => {
+        const album = await getAlbumTracks('4aawyAB9vmqN3uQ7FjRGTy');
         expect(album).toStrictEqual({ data: 'data' });
       });
     });
--- src/libs/spotify-wrapper/album/index.js
+++ src/libs/spotify-wrapper/album/index.js
@@ -4,4 +4,10 @@ export const getAlbum = id =>
   fetch(`https://api.spotify.com/v1/albums/${id}`, { headers: spotifyAccessAuth })
     .then(data => data.json());
 
-export const getAlbumTracks = () => {};
+export const getAlbums = ids => 
+  fetch(`https://api.spotify.com/v1/albums?ids=${ids}`, { headers: spotifyAccessAuth })
+    .then(data => data.json());
+
+export const getAlbumTracks = id => 
+  fetch(`https://api.spotify.com/v1/albums/${id}/tracks`, { headers: spotifyAccessAuth })
+    .then(data => data.json());
